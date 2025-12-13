8a3710aeb5fbcb107eb7440f2d8cca7ed1087983
Heather <danielmertins0@gmail.com>
2025-12-13 18:41:02 +0100

    this one is completely AI, I'm sorry. I don't know yaml nor py

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -1,3 +1,9 @@
 const select = document.getElementById("categories")
 const wrapper = document.getElementById("artWrapper");
 
+console.log("hello freya!!!!");
+
+await fetch('../csv/mydata.csv')
+  .then(r => r.text())
+  .then(txt => txt.split(/\r?\n/))
+  .then(txt => console.log(txt))
