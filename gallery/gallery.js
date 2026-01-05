a9ded69883eedf424a7f5fb3e1f130246ad63d3c
Heather <danielmertins0@gmail.com>
2026-01-05 22:03:24 +0100

    finally got some images working

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -4,7 +4,7 @@ import {make} from "../js/util/injectionUtil.js"
 const select = document.getElementById("categories")
 const wrapper = document.getElementById("artWrapper");
 
-const categories  = await loadJson("../gallery/categories/manifest.json").then(res => res.folders);
+const categories  = await loadJson("../gallery/categories/manifest.json").then(res => Object.keys(res));
 // categories = ["pixelart", "traditional art"]
 categories.forEach(category => 
 	select.add(
@@ -13,9 +13,31 @@ categories.forEach(category =>
 );
 
 async function show(){
+	wrapper.innerHTML="";
 	const currentCategory = select.value;
 	const currentManifest = await loadJson("../gallery/categories/"+currentCategory+"/manifest.json");
-	console.log(currentManifest);
+	for (let key in currentManifest) {
+		const data = currentManifest[key];
+		if (data.type == "image"){
+			const meta = data.meta;
+			const pieceWrapper = make("div",{
+				style:{
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "center"
+				}
+			});
+			pieceWrapper.append(
+				make("h3",{textContent: meta.title}),
+				make("img",{
+					src:"../gallery/categories/"+currentCategory+"/"+key,
+					style:{width:"50%"}
+				})
+			);
+
+			wrapper.append(pieceWrapper);
+		}
+	}
 }
 
 show();
