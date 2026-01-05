55b154c585993549b6ed9b94cc58ec216a946ef8
Heather <danielmertins0@gmail.com>
2026-01-05 22:42:11 +0100

    I hope descriptions work?

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -13,31 +13,47 @@ categories.forEach(category =>
 );
 
 async function show(){
-	wrapper.innerHTML="";
+	wrapper.innerHTML = "";
 	const currentCategory = select.value;
-	const currentManifest = await loadJson("../gallery/categories/"+currentCategory+"/manifest.json");
-	for (let key in currentManifest) {
-		const data = currentManifest[key];
-		if (data.type == "image"){
-			const meta = data.meta;
-			const pieceWrapper = make("div",{
-				style:{
-					display: "flex",
-					flexDirection: "column",
-					alignItems: "center"
-				}
-			});
-			pieceWrapper.append(
-				make("h3",{textContent: meta.title}),
-				make("img",{
-					src:"../gallery/categories/"+currentCategory+"/"+key,
-					style:{width:"50%"}
-				})
-			);
+	const currentManifest = await loadJson(`../gallery/categories/${currentCategory}/manifest.json`);
 
-			wrapper.append(pieceWrapper);
-		}
+  	// Convert manifest object into array of [key, data]
+	const entries = Object.entries(currentManifest)
+		.filter(([key, data]) => data.type === "image")
+		.map(([key, data]) => ({ key, data }));
+
+  	// Parse creation_date and sort ascending (oldest → newest).
+	entries.sort((a, b) => {
+		const da = new Date(a.data.meta.creation_date).getTime();
+		const db = new Date(b.data.meta.creation_date).getTime();
+		return da - db; // use db - da for newest → oldest
+	});
+
+  	// Render in sorted order
+	for (const { key, data } of entries) {
+		const { creation_date, title, description } = data.meta;
+		const pieceWrapper = make("div", {
+			style: {
+				display: "flex",
+				flexDirection: "column",
+				alignItems: "center"
+			}
+		});
+		const desc = document.createElement("p");
+		desc.innerHTML = "\"Life is too short to pretend to be someone you're not.\"\\n Art drawn by @hsse\\n Holiday pet created on June 2024 Holidays!"
+
+		pieceWrapper.append(
+			make("h3", { textContent: title }),
+			make("img", {
+				src: `../gallery/categories/${currentCategory}/${key}`,
+				style: { width: "50%" },
+				alt: title
+			}),
+			desc
+		);
+		wrapper.append(pieceWrapper);
 	}
 }
+
 
 show();
