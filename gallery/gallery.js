3d4d6397db3e1d745ecf64976027afa05ced04ff
Heather <danielmertins0@gmail.com>
2026-01-12 18:09:30 +0100

    adding other categories

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -31,7 +31,7 @@ async function show(){
 
   	// Render in sorted order
 	for (const { key, data } of entries) {
-		const { creation_date, title, description } = data.meta;
+		const { creation_date, title = "untitled", description } = data.meta;
 		const pieceWrapper = make("div", {className:"piece-wrapper"});
 
 		pieceWrapper.append(
