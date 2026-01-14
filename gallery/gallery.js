d4263c28b51de95856f3d41beeafbcd419a5c190
Heather <danielmertins0@gmail.com>
2026-01-14 01:24:08 +0100

    reworked gallery to use google sheets

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -5,8 +5,20 @@ const inputWrapper = document.querySelector(".button-wrapper");
 const [prevButton,select,nextButton] = inputWrapper.children;
 const wrapper = document.getElementById("artWrapper");
 
-const categories  = await loadJson("../gallery/categories/manifest.json").then(res => Object.keys(res));
-// categories = ["pixelart", "traditional art"]
+const URL = [
+	"https://opensheet.elk.sh",
+	"1r_v31yc0E_cZmXlfYO8iFPYet_qYKwQU5t_sBBoTpRQ",
+	"Sheet1"
+].join("/");
+
+var categories = [];
+const ImageArray  = await loadJson(URL);
+
+ImageArray.forEach(image => {
+	if (!categories.includes(image.category))
+		categories.push(image.category);
+});
+
 categories.forEach((category,i) => 
 	select.add(
 		make("option",{text:category, value:i})
@@ -15,56 +27,47 @@ categories.forEach((category,i) =>
 
 var currentIndex = 0;
 
-select.addEventListener("change",()=>{
-		currentIndex = select.value;
-		show();
-});
-prevButton.addEventListener("click",()=>{
-		currentIndex--;
-		if (currentIndex<0) currentIndex = categories.length-1;
-		show();
-});
-nextButton.addEventListener("click",()=>{
-		currentIndex++;
-		if (currentIndex==categories.length) currentIndex = 0;
-		show();
-});
+select.addEventListener("change",()=>changeIndex(+select.value));
+prevButton.addEventListener("click",()=>changeIndex(currentIndex-1));
+nextButton.addEventListener("click",()=>changeIndex(currentIndex+1));
+
+function changeIndex(num){
+	currentIndex = num;
+	if (currentIndex<0) currentIndex = categories.length-1;
+	else if (currentIndex==categories.length) currentIndex = 0;
+	show();
+}
 
 async function show(){
 	wrapper.innerHTML = "";
 	select.text  = categories[currentIndex];
 	select.value = currentIndex;
-	const currentManifest = await loadJson(`categories/${categories[currentIndex]}/manifest.json`);
 
-  	// Convert manifest object into array of [key, data]
-	const entries = Object.entries(currentManifest)
-		.filter(([key, data]) => data.type === "image")
-		.map(([key, data]) => ({ key, data }));
+	const categoryArray = ImageArray.filter(image => image.category == categories[currentIndex]);
 
   	// Parse creation_date and sort ascending (oldest → newest).
-	entries.sort((a, b) => {
-		const da = new Date(a.data.meta.creation_date).getTime();
-		const db = new Date(b.data.meta.creation_date).getTime();
+	categoryArray.sort((a, b) => {
+		const da = new Date(a.creationDate).getTime();
+		const db = new Date(b.creationDate).getTime();
 		return da - db; // use db - da for newest → oldest
 	});
 
   	// Render in sorted order
-	for (const { key, data } of entries) {
-		const { creation_date, description } = data.meta;
-		const title = data.meta.title ?? key;
+	categoryArray.forEach(image => {
+		const { title, creationDate, description, filename } = image;
 		const pieceWrapper = make("div", {className:"piece-wrapper"});
 
 		pieceWrapper.append(
 			make("img", {
-				src: `../gallery/categories/${categories[currentIndex]}/${key}`,
+				src: `../gallery/images/${filename}`,
 				className: "piece-image",
-				alt: title
+				alt: filename
 			}),
 			make("h3", { className:"image-title", textContent: title }),
 			make("p",{ innerHTML:description })
 		);
 		wrapper.append(pieceWrapper);
-	}
+	});
 }
 
 show();
