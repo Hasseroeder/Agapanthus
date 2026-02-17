7a371e5b8657aef33f740d2e9aba33db7b2727cd
Heather <danielmertins0@gmail.com>
2026-02-17 22:53:24 +0100

    broken due to passive json being not ready. Will do that right now

--- neon/neon.js
+++ neon/neon.js
@@ -7,7 +7,21 @@ const page = document.getElementById("page");
 const pageSize = document.getElementById("pageSize");
 [page, pageSize].forEach(el => el.addEventListener("change", render))
 
-const {tiers, passives, weapons} = await loadJson("../neon/data.json");
+const [weapons, passives, buffs] = await Promise.all([
+    loadJson("media/json/weapons.json"),
+    loadJson("media/json/passives.json"),
+    loadJson("media/json/buffs.json")
+]);
+
+const tiers =[
+    {"min":0,  "letter":"c", "name":"common"},
+    {"min":20, "letter":"u", "name":"uncommon"},
+    {"min":40, "letter":"r", "name":"rare"},
+    {"min":60, "letter":"e", "name":"epic"},
+    {"min":80, "letter":"m", "name":"mythic"},
+    {"min":94, "letter":"l", "name":"legendary"},
+    {"min":100,"letter":"f", "name":"fabled"}
+];
 
 const table = document.getElementById("weaponTable");
 const reader = new FileReader();
@@ -85,31 +99,38 @@ function createTable(array) {
     table.innerHTML = "";
 
     const header = table.insertRow();
-    ["ID", "Type", "Quality", "Blueprint", "Passive"].forEach(
+    ["ID","Image", "Type", "Quality", "Blueprint"/*, "Passive"*/].forEach(
         text => header.appendChild(make("th",{textContent:text}))
     );
 
     array.forEach(w => {
+        const weaponData = weapons[w.t];
         const row = table.insertRow();
 
         const IDcell = row.insertCell();
         IDcell.append(make("code",{className:"id-code", textContent: w.id}));
 
-        row.insertCell().textContent = 100 + w.t + " - " + capitalizeFirstLetter(weapons[w.t]);
+        const imageCell = row.insertCell();
+        w.p.forEach(p=>{
+            const tier = tiers[p.r];
+            const staticData = passives[p.t-1];
+            imageCell.append(
+                make("img", {
+                    src:"../neon/media/battleEmojis/"+tier.letter+"_"+staticData.slug+".png",
+                    style:"height:1.2rem"
+                })
+            )
+        });
+
+        row.insertCell().textContent = 100 + w.t + " - " + capitalizeFirstLetter(weaponData.slug);
         row.insertCell().textContent = w.q + "%";
         row.insertCell().textContent = w.bp;
 
+        /*
         const passiveCell = row.insertCell();
-        w.p.forEach(p=>
-            passiveCell.append(
-            make("img", {
-                src:"../neon/media/passives/"+tiers[p.r].letter+"_"+passives[p.t]+".png",
-                style:"height:1.2rem"
-            })
-        ));
-
         const passiveText = w.p
             .map(p => tiers[p.r].name + " " + capitalizeFirstLetter(passives[p.t]))
             .join(" | ");
+        */
     });
 }
