11ee7b78b00aa1bb53d18eca9f9aae224f245866
Heather <148720088+Hasseroeder@users.noreply.github.com>
2026-05-29 17:31:29 +0200

    style: added the html for my searchbar. no js yet.

--- startpage/startpage.js
+++ startpage/startpage.js
@@ -1,3 +1,6 @@
+import { loadJson } from "/js/util/jsonUtil.js";
+import { make } from "/js/util/injectionUtil.js";
+
 const banner = document.getElementById("banner");
 
 const konachanReq = await fetch("https://antix1.transaero.space/api/", {
@@ -9,3 +12,20 @@ const konachanReq = await fetch("https://antix1.transaero.space/api/", {
 const bannerSrc = await konachanReq.json();
 banner.referrerPolicy = "no-referrer";
 banner.src = bannerSrc;
+
+const engineGrid = document.getElementById("engine-grid");
+const engines = await loadJson("/startpage/media/engine.json");
+engines.forEach((engine) => {
+    const wrapper = make("div", { className: "engine-wrapper" });
+    const { xmlns, viewBox } = engine.icon.svg;
+    const svg = document.createElementNS(xmlns, "svg");
+    svg.setAttribute("viewBox", viewBox);
+    svg.setAttribute("xmlns", xmlns);
+    const { d } = engine.icon.path;
+    const path = document.createElementNS(xmlns, "path");
+    path.setAttribute("d", d);
+    path.setAttribute("fill", "#808080");
+    svg.append(path);
+    wrapper.append(svg);
+    engineGrid.append(wrapper);
+});
