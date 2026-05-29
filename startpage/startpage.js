a8b4b270414e8076c9e3a590e6a172733f88aa23
Heather <148720088+Hasseroeder@users.noreply.github.com>
2026-05-29 11:51:07 +0200

    sobb

--- startpage/startpage.js
+++ startpage/startpage.js
@@ -6,8 +6,9 @@ const konachanReq = await fetch("https://antix1.transaero.space/api/", {
         "x-api-key": "my_super_duper_mega_ultra_secure_API_key",
     },
 });
-
 const bannerSrc = await konachanReq.json();
+banner.crossOrigin = "anonymous";
+banner.referrerPolicy = "no-referrer";
 banner.src = bannerSrc;
 
 banner.onerror = () => {
