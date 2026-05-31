8857cebad59279926c3742ee43c3bd484b597d63
Heather <148720088+Hasseroeder@users.noreply.github.com>
2026-05-31 16:40:00 +0200

    feat: added more browser fingerprinting and clock elements

--- startpage/startpage.js
+++ startpage/startpage.js
@@ -1,8 +1,7 @@
 import { loadJson } from "/js/util/jsonUtil.js";
 import { make } from "/js/util/injectionUtil.js";
 
-const banner = document.getElementById("banner");
-
+const fetchImage = document.getElementById("fastfetch-image");
 const konachanReq = await fetch("https://antix1.transaero.space/api/", {
     method: "GET",
     headers: {
@@ -11,7 +10,7 @@ const konachanReq = await fetch("https://antix1.transaero.space/api/", {
 });
 const bannerSrc = await konachanReq.json();
 banner.referrerPolicy = "no-referrer";
-banner.src = bannerSrc;
+//fetchImage.src = "/startpage/media/temporary.jpg";
 
 const engineGrid = document.getElementById("engine-grid");
 const engines = await loadJson("/startpage/media/engine.json");
@@ -29,3 +28,99 @@ engines.forEach((engine) => {
     wrapper.append(svg);
     engineGrid.append(wrapper);
 });
+
+function updateFingerprinting(extraUserInfo) {
+    const userInfo = bowser.getParser(window.navigator.userAgent).parsedResult;
+    Object.assign(userInfo, {
+        language: navigator.language || navigator.userLanguage,
+        ...extraUserInfo,
+    });
+
+    const hostname =
+        userInfo.userName +
+        "@" +
+        userInfo.browser.name.toLowerCase() +
+        "-" +
+        userInfo.browser.version;
+
+    Array.from(document.querySelectorAll(".hostname")).forEach(
+        (el) => (el.textContent = hostname),
+    );
+    Array.from(document.querySelectorAll(".fastfetch-separator")).forEach(
+        (el) => (el.textContent = "─".repeat(hostname.length)),
+    );
+
+    const OSicon =
+        {
+            linux: "󰌽",
+            android: "󰀲",
+            windows: "󰨡",
+            ios: "",
+            macos: "",
+        }[userInfo.os.name.toLowerCase()] ?? "";
+
+    const languageIcon = "";
+
+    document.querySelector(".locale").textContent =
+        languageIcon + "  Locale ⇀ " + userInfo.language;
+    document.querySelector(".os").textContent =
+        OSicon +
+        "  OS     ⇀ " +
+        userInfo.platform.type +
+        " " +
+        userInfo.os.name.toLowerCase();
+}
+
+const clocks = Array.from(document.querySelectorAll(".clock"));
+
+const timezones = [
+    {
+        airport: "SLC",
+        timeZone: "America/Denver",
+    },
+    {
+        airport: "NYC",
+        timeZone: "America/New_York",
+    },
+    {
+        airport: "BER",
+        timeZone: "Europe/Berlin",
+    },
+];
+
+timezones.forEach((tz, i) => {
+    const clocks = Array.from(document.querySelectorAll(".clock"));
+    const local = new Date();
+    const utc = new Date(local.toLocaleString("en-US", { timeZone: "UTC" }));
+    const zoned = new Date(local.toLocaleString("en-US", tz));
+    tz.utcOffset = (zoned - utc) / 60000 / 60;
+    tz.hour12 = false;
+    tz.hour = "2-digit";
+    tz.minute = "2-digit";
+    tz.clock = clocks[i];
+
+    const region = tz.timeZone.split("/")[0];
+    const globeIcon =
+        {
+            Africa: "",
+            America: "",
+            Asia: "",
+            Europe: "",
+        }[region] ?? "󰊷";
+
+    tz.update = function () {
+        const beforeUtc = tz.utcOffset < 0;
+        const absUtcOffset = Math.abs(tz.utcOffset);
+        const utcTimeString =
+            (beforeUtc ? "+" : "-") + String(absUtcOffset).padStart(2, "0");
+
+        const airportString = (globeIcon + " " + tz.airport).padEnd(7, " ");
+        const utcString = ("UTC" + utcTimeString).padEnd(8, " ");
+        const timeString = new Date().toLocaleTimeString("en-US", tz);
+
+        tz.clock.textContent = airportString + utcString + timeString;
+    };
+    tz.update();
+});
+
+updateFingerprinting({ userName: "heather" });
